41269510cef3a77a17466aee03851bda0f38ae5b
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2021-03-05 16:54:17 -0500

    uncommented cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
  get '/guides', to: "user_guides#index"
- get '/character', to: "character#index"
+ get '/characters', to: "characters#index"
  
  get '/guides/:id', to: "user_guides#show"
  get '/characters/:id', to: "characters#show"
